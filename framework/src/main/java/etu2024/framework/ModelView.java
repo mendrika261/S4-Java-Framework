4c8c692cd6572e21a22bc31fe425a1a8a0d310b1
mendrika <ramialison.mendrika@icloud.com>
2023-03-30 19:36:35 +0300

    add ModelView

---
+++ framework/src/main/java/etu2024/framework/ModelView.java
@@ -0,0 +1,17 @@
+package etu2024.framework;
+
+public class ModelView {
+    String view;
+
+    public ModelView(String view) {
+        setView(view);
+    }
+
+    public String getView() {
+        return view;
+    }
+
+    public void setView(String view) {
+        this.view = view;
+    }
+}
